a4cdfd2edfb1c3e952f9f7898a3dd8eea7e26003
김재명 (AI 팀) <kimjaemyung@hodoo-05.hodoo.ai>
2019-08-09 20:06:47 +0900

    ddpg added

---
+++ ddpg/agent.py
@@ -0,0 +1,140 @@
+import os
+import tensorflow as tf
+import numpy as np
+
+from environment import MnistEnvironment, Environment
+from ddpg.replay_memory import ReplayMemory
+from ddpg.ddpg_model import DDPG
+from origin_model.mnist_solver import Network
+
+class Agent(object):
+    def __init__(self, args, sess):
+        # CartPole 환경
+        self.sess = sess
+        self.model = Network(sess, phase='train') # mnist accurcacy model
+        self.env = MnistEnvironment(self.model) 
+        self.state_size = self.env.state_size
+        self.action_size = self.env.action_size
+        self.a_bound = self.env.a_bound
+        self.train_size = len(self.env.train_images)
+        self.test_size = len(self.env.test_images)
+        self.learning_rate = args.learning_rate
+        self.batch_size = args.batch_size
+        self.discount_factor = args.discount_factor
+        self.epochs = args.epochs
+        self.ENV = Environment(self.env, self.state_size, self.action_size)
+        self.replay = ReplayMemory(self.state_size, self.batch_size)
+        self.ddpg = DDPG(self.state_size, self.action_size, self.sess, self.learning_rate, 
+                         self.replay, self.discount_factor, self.a_bound)
+
+        self.continue_train = args.continue_train
+        self.save_dir = args.save_dir
+        self.render_dir = args.render_dir
+        self.play_dir = args.play_dir
+
+        # initialize
+        sess.run(tf.global_variables_initializer())  # tensorflow graph가 다 만들어지고 난 후에 해야됨
+
+        # load pre-trained mnist model
+        self.env.model.checkpoint_load()
+        
+        self.saver = tf.train.Saver()
+
+        # continue_train
+        if self.continue_train:
+            self.load()
+
+        self.epsilon = 1
+        self.explore = 2e4
+        pass
+
+    '''
+    def select_action(self, state):
+        return np.clip(
+            np.random.normal(self.sess.run(self.ddpg.actor, {self.ddpg.state: state})[0], self.action_variance), -2,
+            2)
+        pass
+    '''
+
+    def ou_function(self, mu, theta, sigma):
+        x = np.ones(self.action_size) * mu
+        dx = theta * (mu - x) + sigma * np.random.randn(self.action_size)
+        return x + dx
+
+    def noise_select_action(self, state):
+        action = self.sess.run(self.ddpg.actor, {self.ddpg.state: state})[0]
+        noise = self.epsilon * self.ou_function(0, 0.15, 0.25)
+        return action + noise
+
+    def select_action(self, state):
+        return self.sess.run(self.ddpg.actor, {self.ddpg.state: state})[0]
+
+    def train(self):
+        scores, episodes = [], []
+        for e in range(self.epochs):
+            for i, idx in enumerate(np.random.permutation(self.train_size)):
+                terminal = False
+                score = 0
+                state = self.ENV.new_episode(idx)
+                state = np.reshape(state, [1, self.state_size])
+
+                while not terminal:
+                    action = self.noise_select_action(state)
+                    next_state, reward, terminal = self.ENV.act(action)
+                    state = state[0]
+                    self.replay.add(state, action, reward, next_state, terminal)
+    
+                    if len(self.replay.memory) >= self.batch_size:
+                        self.ddpg.update_target_network()
+                        self.ddpg.train_network()
+    
+                    score += reward
+                    state = np.reshape(next_state, [1, self.state_size])
+    
+                    if terminal:
+                        scores.append(score)
+                        episodes.append(e)
+                        if (i+1)%50 == 0:
+                            print('epoch', e+1, 'iter:', f'{i+1:05d}', ' score:', f'{score:.03f}', ' last 10 mean score', f'{np.mean(scores[-min(10, len(scores)):]):.03f}', f'sequence: {self.env.sequence}')
+                        if (i+1)%50 == 0:
+                            self.ENV.render_worker(os.path.join(self.render_dir, f'{(i+1):05d}.png'))
+                        if (i+1)%1000 == 0:
+                            self.save()
+
+        pass
+
+    def play(self):
+        cor_before_lst, cor_after_lst = [], []
+        for idx in range(self.test_size): 
+            state = self.ENV.new_episode(idx, phase='test')
+            state = np.reshape(state, [1, self.state_size])
+    
+            terminal = False
+            score = 0
+            while not terminal:
+                action = self.select_action(state)
+                next_state, reward, terminal = self.ENV.act(action)
+                next_state = np.reshape(next_state, [1, self.state_size])
+                score += reward
+                state = next_state
+#                 time.sleep(0.02)
+                if terminal:
+                    (cor_before, cor_after) = self.ENV.compare_accuracy()
+                    cor_before_lst.append(cor_before)
+                    cor_after_lst.append(cor_after)
+
+                    self.ENV.render_worker(os.path.join(self.play_dir, f'{(idx+1):04d}.png'))
+                    print(f'{(idx+1):04d} image score: {score}\n')
+        print('====== NUMBER OF CORRECTION =======')
+        print(f'before: {np.sum(cor_before_lst)}, after: {np.sum(cor_after_lst)}')
+    pass
+
+    def save(self):
+        checkpoint_dir = os.path.join(self.save_dir, 'ckpt')
+        if not os.path.exists(checkpoint_dir):
+            os.mkdir(checkpoint_dir)
+        self.saver.save(self.sess, os.path.join(checkpoint_dir, 'trained_agent'))
+
+    def load(self):
+        checkpoint_dir = os.path.join(self.save_dir, 'ckpt')
+        self.saver.restore(self.sess, os.path.join(checkpoint_dir, 'trained_agent'))
---
+++ origin_model/mnist_model.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+
+
+def classifier(images, options, reuse=False, name='classifier'):
+    with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
+        x = relu(conv2d(images, options.nf, ks=5, s=1, name='conv1')) # 28*28*nf
+        x = relu(conv2d(x, 2*options.nf, ks=3, s=2, name='conv2')) # 14*14*(2*nf)
+        x = relu(conv2d(x, 4*options.nf, ks=3, s=2, name='conv3')) # 7*7*(4*nf)
+
+        x = linear(tf.reshape(x, [-1, 7*7*(4*options.nf)]), 128, name='linear1')
+        x = dropout(x, 0.5, options.phase)
+        x = linear(x, options.label_n, name='linear2')
+        return x
+
+
+def cls_loss(logits, labels):
+    return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,labels=labels))
+
+
+
+def conv2d(input_, output_dim, ks=3,s=1,padding='SAME',name='conv2d'):
+    with tf.variable_scope(name):
+        return slim.conv2d(input_, output_dim, ks, s, padding=padding,
+                           weights_initializer=tf.contrib.layers.xavier_initializer())
+     
+def linear(input_, output_dim, stddev=0.02, name='linear'):
+    with tf.variable_scope(name):
+        return slim.fully_connected(input_, output_dim, activation_fn=None,
+                                    weights_initializer=tf.contrib.layers.xavier_initializer())
+
+def dropout(x, rate, phase):
+    if phase is 'train':
+        phase = True
+    else:
+        phase = False
+    return tf.layers.dropout(inputs=x, rate=rate, training=phase)
+
+
+def average_pooling(x, ks=[2,2], s=2, padding='VALID'):
+    return tf.layers.average_pooling2d(inputs=x, pool_size=ks, strides=s, padding=padding)
+
+
+def max_pooling(x, ks=[3,3], s=2, padding='VALID'):
+    return tf.layers.max_pooling2d(inputs=x, pool_size=ks, strides=s, padding=padding)
+
+def flatten(x):
+    return tf.contrib.layers.flatten(x)
+
+def relu(x, name='relu'):
+    return tf.nn.relu(x)
